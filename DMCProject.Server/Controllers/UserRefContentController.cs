f8aff06b0352dcda0ac19efd7870c589ef17415a
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2024-03-15 01:09:52 -0400

    Add backend handling (folder, database) for UR content

--- DMCProject.Server/Controllers/UserRefContentController.cs
+++ DMCProject.Server/Controllers/UserRefContentController.cs
@@ -20,8 +20,9 @@ namespace DMCProject.Server.Controllers
 
         [HttpPost]
         [Route("AddURContent")]
-        public void AddURContent([FromBody] JsonElement jsonData)
+        public IActionResult AddURContent([FromBody] JsonElement jsonData)
         {
+            string msg = "";
             if(jsonData.ValueKind == JsonValueKind.Object)
             {
                 System.Diagnostics.Debug.WriteLine(jsonData);
@@ -36,14 +37,43 @@ namespace DMCProject.Server.Controllers
                 MySqlConnection conn = test.ConnectDB();
                 MySqlCommand cmd = conn.CreateCommand();
 
-/*                cmd.CommandText = "INSERT INTO AddURContent (FirstName, MiddleName, LastName, ImagePath, ImageCategory) VALUES (@value1, @value2, @value3, @value4, @value5)";
+                System.Diagnostics.Debug.WriteLine(firstName);
+                System.Diagnostics.Debug.WriteLine(uploadImage);
+                string filePath = handleImage(uploadImage);
+
+                cmd.CommandText = "INSERT INTO AddURContent (FirstName, MiddleName, LastName, FilePath, ImageCategory) VALUES (@value1, @value2, @value3, @value4, @value5)";
                 cmd.Parameters.AddWithValue("@value1", firstName);
                 cmd.Parameters.AddWithValue("@value2", middleName);
                 cmd.Parameters.AddWithValue("@value3", lastName);
-                cmd.Parameters.AddWithValue("@value4", category);
-                cmd.ExecuteNonQuery();*/
+                cmd.Parameters.AddWithValue("@value4", filePath);
+                cmd.Parameters.AddWithValue("@value5", category);
+                cmd.ExecuteNonQuery();
+
+                msg = "Your submission was submitted for review!";
+                System.Diagnostics.Debug.WriteLine(msg);
+
                 test.CloseDB(conn);
             }
+            return Ok(msg);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public String handleImage(string dataUrl)
+        {
+            // Split the dataUrl to get the base64
+            string base64Str = dataUrl.Split(",").Last();
+
+            // Convert the base64 string to a byte array
+            byte[] bytes = Convert.FromBase64String(base64Str);
+
+            string filePath = Path.Combine("customPictures", Guid.NewGuid().ToString() + ".png" );
+
+            // Write the byte array to a filePath
+            System.IO.File.WriteAllBytes(filePath, bytes);
+
+            System.Diagnostics.Debug.WriteLine("Image saved to " + filePath);
+
+            return filePath;
         }
     }
 }
