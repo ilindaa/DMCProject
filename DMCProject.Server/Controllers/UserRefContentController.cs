c32c457932a31e10ffd2b37bf43281a70d22f41a
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2024-03-21 23:07:54 -0400

    Add AddURContent displayed in a table

--- DMCProject.Server/Controllers/UserRefContentController.cs
+++ DMCProject.Server/Controllers/UserRefContentController.cs
@@ -10,7 +10,8 @@ namespace DMCProject.Server.Controllers
     [EnableCors("_myAllowSpecificOrigins")]
     [ApiController]
     [Route("api/[controller]")]
-    public class UserRefContentController : SessionController {
+    public class UserRefContentController : SessionController
+    {
 
         [ApiExplorerSettings(IgnoreApi = true)]
         public ActionResult Index()
@@ -23,49 +24,56 @@ namespace DMCProject.Server.Controllers
         public IActionResult AddURContent([FromBody] JsonElement jsonData)
         {
             string msg = "";
-            if(jsonData.ValueKind == JsonValueKind.Object)
+            if (jsonData.ValueKind == JsonValueKind.Object)
             {
-                System.Diagnostics.Debug.WriteLine(jsonData);
-                JObject jo = JsonConvert.DeserializeObject<JObject>(jsonData.GetRawText());
-                string firstName = jo["firstName"].ToString();
-                string middleName = jo["middleName"].ToString();
-                string lastName = jo["lastName"].ToString();
-                string uploadImage = jo["uploadImage"].ToString();
-                string category = jo["category"].ToString();
+                try
+                {
+                    System.Diagnostics.Debug.WriteLine(jsonData);
+                    JObject jo = JsonConvert.DeserializeObject<JObject>(jsonData.GetRawText());
+                    string firstName = jo["firstName"].ToString();
+                    string middleName = jo["middleName"].ToString();
+                    string lastName = jo["lastName"].ToString();
+                    string uploadImage = jo["uploadImage"].ToString();
+                    string category = jo["category"].ToString();
 
-                ConnectionTest test = new ConnectionTest();
-                MySqlConnection conn = test.ConnectDB();
-                MySqlCommand cmd = conn.CreateCommand();
+                    ConnectionTest test = new ConnectionTest();
+                    MySqlConnection conn = test.ConnectDB();
+                    MySqlCommand cmd = conn.CreateCommand();
 
-                System.Diagnostics.Debug.WriteLine(firstName);
-                System.Diagnostics.Debug.WriteLine(uploadImage);
-                string filePath = handleImage(uploadImage);
+                    System.Diagnostics.Debug.WriteLine(firstName);
+                    System.Diagnostics.Debug.WriteLine(uploadImage);
+                    string filePath = handleImage(uploadImage);
 
-                // Insert into the AddURContent table
-                cmd.CommandText = "INSERT INTO AddURContent (FirstName, MiddleName, LastName, FilePath, ImageCategory) VALUES (@value1, @value2, @value3, @value4, @value5)";
-                cmd.Parameters.AddWithValue("@value1", firstName);
-                cmd.Parameters.AddWithValue("@value2", middleName);
-                cmd.Parameters.AddWithValue("@value3", lastName);
-                cmd.Parameters.AddWithValue("@value4", filePath);
-                cmd.Parameters.AddWithValue("@value5", category);
-                cmd.ExecuteNonQuery();
+                    // Insert into the AddURContent table
+                    cmd.CommandText = "INSERT INTO AddURContent (FirstName, MiddleName, LastName, FilePath, ImageCategory) VALUES (@value1, @value2, @value3, @value4, @value5)";
+                    cmd.Parameters.AddWithValue("@value1", firstName);
+                    cmd.Parameters.AddWithValue("@value2", middleName);
+                    cmd.Parameters.AddWithValue("@value3", lastName);
+                    cmd.Parameters.AddWithValue("@value4", filePath);
+                    cmd.Parameters.AddWithValue("@value5", category);
+                    cmd.ExecuteNonQuery();
 
-                // Select the last insert id
-                cmd.CommandText = "SELECT LAST_INSERT_ID()";
-                int lastInsertId = Convert.ToInt32(cmd.ExecuteScalar());
+                    // Select the last insert id
+                    cmd.CommandText = "SELECT LAST_INSERT_ID()";
+                    int lastInsertId = Convert.ToInt32(cmd.ExecuteScalar());
 
-/*                int sessionId = -1;*/
-                // Insert into the ReviewURContent table
-                cmd.CommandText = "INSERT INTO ReviewURContent (AddURContentID) VALUES (@value6)";
-                /*                cmd.CommandText = "INSERT INTO ReviewURContent (AddURContentID, AccountID) VALUES (@value6, @value7)";*/
-                cmd.Parameters.AddWithValue("@value6", lastInsertId);
-/*                cmd.Parameters.AddWithValue("@value7", sessionId);*/
-                cmd.ExecuteNonQuery();
+                    /*                int sessionId = -1;*/
+                    // Insert into the ReviewURContent table
+                    cmd.CommandText = "INSERT INTO ReviewURContent (AddURContentID) VALUES (@value6)";
+                    /*                cmd.CommandText = "INSERT INTO ReviewURContent (AddURContentID, AccountID) VALUES (@value6, @value7)";*/
+                    cmd.Parameters.AddWithValue("@value6", lastInsertId);
+                    /*                cmd.Parameters.AddWithValue("@value7", sessionId);*/
+                    cmd.ExecuteNonQuery();
 
-                msg = "Submitted for review!";
-                System.Diagnostics.Debug.WriteLine(msg);
+                    msg = "Submitted for review!";
+                    System.Diagnostics.Debug.WriteLine(msg);
 
-                test.CloseDB(conn);
+                    test.CloseDB(conn);
+                }
+                catch (Exception error)
+                {
+                    System.Diagnostics.Debug.WriteLine(error);
+                }
             }
             return Ok(msg);
         }
@@ -73,20 +81,69 @@ namespace DMCProject.Server.Controllers
         [ApiExplorerSettings(IgnoreApi = true)]
         public String handleImage(string dataUrl)
         {
-            // Split the dataUrl to get the base64
-            string base64Str = dataUrl.Split(",").Last();
+            string filePath = "";
+            try
+            {
+                // Split the dataUrl to get the base64
+                string base64Str = dataUrl.Split(",").Last();
 
-            // Convert the base64 string to a byte array
-            byte[] bytes = Convert.FromBase64String(base64Str);
+                // Convert the base64 string to a byte array
+                byte[] bytes = Convert.FromBase64String(base64Str);
 
-            string filePath = Path.Combine("customPictures", Guid.NewGuid().ToString() + ".png" );
+                filePath = Path.Combine("customPictures", Guid.NewGuid().ToString() + ".png");
 
-            // Write the byte array to a filePath
-            System.IO.File.WriteAllBytes(filePath, bytes);
-
-            System.Diagnostics.Debug.WriteLine("Image saved to " + filePath);
+                // Write the byte array to a filePath
+                System.IO.File.WriteAllBytes(filePath, bytes);
+                System.Diagnostics.Debug.WriteLine("Image saved to " + filePath);
+            }
+            catch (Exception error)
+            {
+                System.Diagnostics.Debug.WriteLine(error);
+            }
 
             return filePath;
         }
+
+        [HttpGet]
+        [Route("GetAddURContent")]
+        public IActionResult GetAddURContent()
+        {
+            ConnectionTest test = new ConnectionTest();
+            MySqlConnection conn = test.ConnectDB();
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM AddURContent";
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            List<MyData> dataList = new List<MyData>();
+
+            while (rdr.Read())
+            {
+                MyData data = new MyData();
+                data.addURContentID = Convert.ToInt32(rdr["AddURContentID"]);
+                data.firstName = rdr["FirstName"].ToString();
+                data.middleName = rdr["MiddleName"].ToString();
+                data.lastName = rdr["LastName"].ToString();
+                data.filePath = rdr["FilePath"].ToString();
+                data.imageCategory = rdr["ImageCategory"].ToString();
+                dataList.Add(data);
+            }
+
+            test.CloseDB(conn);
+
+            string json = JsonConvert.SerializeObject(dataList);
+
+            return Content(json, "application/json");
+        }
+    }
+
+    public class MyData
+    {
+        public int addURContentID;
+        public string firstName;
+        public string middleName;
+        public string lastName;
+        public string filePath;
+        public string imageCategory;
     }
 }
--- dmcproject.client/src/ApproveURContent.tsx
+++ dmcproject.client/src/ApproveURContent.tsx
@@ -1,10 +1,94 @@
 import { Link } from "react-router-dom";
-/*import { FC, useEffect } from "react";*/
+import { FC, useEffect } from "react";
 
-function ApproveURContent() {
+const Approve: FC = () => {
+    useEffect(() => {
+
+        try {
+            fetch("https://localhost:7035/api/UserRefContent/GetAddURContent", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then(response => {
+                console.log(response);
+                return response.text();
+            }).then(jsonData => {
+                createTable(jsonData);
+                console.log(jsonData);
+            }).catch(error => {
+                console.log(error);
+            })
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, []);
     return (<>
         <h1>Approve Content</h1>
         <Link to="../admin-page">Back to Admin</Link>
+    </>);
+}
+
+function createTable(jsonData: string) {
+    const dataObject: MyData[] = JSON.parse(jsonData);
+    const dataTableBody = document.getElementById("dataTableBody");
+
+/*    const columnLength = Object.keys(dataObject).length;*/
+/*  console.log(dataObject[0]["addURContentID"]);*/
+    for (let i = 0; i < dataObject.length; i++) {
+        const tr = document.createElement("tr");
+        dataTableBody.appendChild(tr);
+
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const td4 = document.createElement("td");
+        const td5 = document.createElement("td");
+        const td6 = document.createElement("td");
+
+        td1.innerText = dataObject[i]["addURContentID"].toString();
+        td2.innerText = dataObject[i]["firstName"];
+        td3.innerText = dataObject[i]["middleName"];
+        td4.innerText = dataObject[i]["lastName"];
+        td5.innerText = dataObject[i]["filePath"];
+        td6.innerText = dataObject[i]["imageCategory"];
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+    } 
+}
+
+interface MyData {
+    addURContentID: number;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    filePath: string;
+    imageCategory: string;
+}
+
+function ApproveURContent() {
+    return (<>
+        <Approve />
+        <div id="tableDiv">
+            <table id="dataTable">
+                <tbody id="dataTableBody">
+                    <tr>
+                        <th>AddURContentID</th>
+                        <th>FirstName</th>
+                        <th>MiddleName</th>
+                        <th>LastName</th>
+                        <th>FilePath</th>
+                        <th>ImageCategory</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </>);
 }
 
